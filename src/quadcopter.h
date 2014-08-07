c2c3980b8b0f2bfa504d55c80b262cb5cb49e133
Shreyas <skraman1999@gmail.com>
2014-08-07 15:40:27 -0700

    Final motor pins.

--- src/quadcopter.h
+++ src/quadcopter.h
@@ -1,15 +1,15 @@
 #include "simpletools.h"
 
-#define PIN_IMU_SDA    12
-#define PIN_IMU_SCL    11
-#define PIN_MOTOR_FR   4
-#define PIN_MOTOR_FL   5
-#define PIN_MOTOR_BR   6
-#define PIN_MOTOR_BL   7
-#define PIN_XBEE_IN    15 // DOUT
-#define PIN_XBEE_OUT   14 // DIN
-#define PIN_RANGE_TRIG 7
-#define PIN_RANGE_ECHO 8
+//#define PIN_IMU_SDA    12
+//#define PIN_IMU_SCL    11
+#define PIN_MOTOR_FR   9
+#define PIN_MOTOR_FL   10
+#define PIN_MOTOR_BR   8
+#define PIN_MOTOR_BL   11
+//#define PIN_XBEE_IN    15 // DOUT
+//#define PIN_XBEE_OUT   14 // DIN
+//#define PIN_RANGE_TRIG 7
+//#define PIN_RANGE_ECHO 8
 
 #define GYRO_ADDR  0xD0
 #define GYRO_REG_X 0x1D
