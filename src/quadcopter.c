97d6bdd9264088a41a1a7055a42a7a507374dfc2
Shreyas <skraman1999@gmail.com>
2014-08-14 17:20:30 -0700

    We're going all in.

--- src/quadcopter.c
+++ src/quadcopter.c
@@ -5,10 +5,10 @@ int main()
 {
   motor_init();
   xbee_init();
-  //imu_init();
+  imu_init();
   
-  //cog_run(&imu_run, 40);
+  cog_run(&imu_run, 40);
   cog_run(&xbee_run, 10);
-  //cog_run(&ultrasonic_run, 10);
+  cog_run(&ultrasonic_run, 10);
   motor_run();
 }
